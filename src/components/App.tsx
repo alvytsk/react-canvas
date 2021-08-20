55cf39e78ec859877197213923b60caede93a6df
Alexey Vymyatnin <vymyatnin_av@lemz-t.ru>
2021-08-20 09:57:13 +0700

    Added conva and react-konva. Basic drawings

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,50 +1,62 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./App.less";
-import Canvas from "./Canvas";
+import { Stage, Layer, Rect, Text, Circle, Line, Group } from "react-konva";
 
 const App = () => {
-  function roundedRect(
-    ctx: CanvasRenderingContext2D,
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-    radius: number
-  ) {
-    ctx.beginPath();
-    ctx.moveTo(x, y + radius);
-    ctx.lineTo(x, y + height - radius);
-    ctx.arcTo(x, y + height, x + radius, y + height, radius);
-    ctx.lineTo(x + width - radius, y + height);
-    ctx.arcTo(x + width, y + height, x + width, y + height - radius, radius);
-    ctx.lineTo(x + width, y + radius);
-    ctx.arcTo(x + width, y, x + width - radius, y, radius);
-    ctx.lineTo(x + radius, y);
-    ctx.arcTo(x, y, x, y + radius, radius);
-    ctx.stroke();
-  }
+  const [stateColor, setStateColor] = useState<string>("red");
+  const stateRef = useRef(stateColor);
+  stateRef.current = stateColor;
 
-  const draw = (ctx: any, frameCount: number) => {
-    // console.log(ctx, frameCount);
-
-    // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    // ctx.fillStyle = "#ff0000";
-    // ctx.beginPath();
-    // ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
-    // ctx.fill();
-
-    roundedRect(ctx, 12, 12, 200, 100, 5);
-    ctx.fillStyle = "green";
-    ctx.fill();
-    ctx.fillStyle = "white";
-    ctx.font = "20px serif";
-    ctx.fillText("Hello world", 62, 95);
-  };
+  useEffect(() => {
+    const timer = setInterval(function () {
+      setStateColor(stateRef.current === "red" ? "green" : "red");
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className="app">
-      <p />
-      <Canvas draw={draw} />
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Rect
+            id="test1"
+            x={20}
+            y={50}
+            width={200}
+            height={50}
+            fill={stateColor}
+            shadowBlur={6}
+            shadowOpacity={0.6}
+            scaleX={1.0}
+            scaleY={1.0}
+            onClick={(event) => {
+              alert(`${event.target.attrs.id} clicked`);
+            }}
+          />
+          <Text
+            x={24}
+            y={55}
+            fill="white"
+            text="Some text on canvas"
+            fontSize={15}
+          />
+          <Rect
+            id="test2"
+            x={20}
+            y={120}
+            width={200}
+            height={50}
+            fill="orange"
+            shadowBlur={6}
+            shadowOpacity={0.6}
+            scaleX={1.0}
+            scaleY={1.0}
+            onClick={(event) => {
+              alert(`${event.target.attrs.id} clicked`);
+            }}
+          />
+        </Layer>
+      </Stage>
     </div>
   );
 };
